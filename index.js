089fbf0f822411fdeb3c5c13dab4453c741a0726
方正 <215566435@qq.com>
2018-02-05 23:49:39 +1300

    添加model支持

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const Sequelize = require('sequelize');
 
 module.exports = (app) => {
     const { database, username, password, ...options } = app.config.sequelize;
-    app.sequelize = new Sequelize(database, username, password, options);
-
+    app.Sequelize = new Sequelize(database, username, password, options);
+    app.model = app.Sequelize.model
+    loadModel(app);
 }
 
 
