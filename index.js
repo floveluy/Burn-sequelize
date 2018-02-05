aecb4b463691cf7926a72adc7cc2cea90bf013b5
方正 <215566435@qq.com>
2018-02-05 23:46:56 +1300

    添加测试模型

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+const { readdirSync } = require('fs');
 const Sequelize = require('sequelize');
 
 module.exports = (app) => {
-    const { database, username, password,..options } = app.config.sequelize;
+    const { database, username, password, ...options } = app.config.sequelize;
     app.sequelize = new Sequelize(database, username, password, options);
+
 }
+
+
+function loadModel(app) {
+    const index = __dirname.indexOf('node_modules');
+    if (index > 0) {
+        const currentDir = __dirname.substr(0, index);
+        readdirSync(currentDir + '/model').forEach((filename) => {
+            const module = require(currentDir + '/model/' + filename).default;
+
+
+        })
+
+    } else {
+        readdirSync('./model').forEach((filename) => {
+            const moduleFunction = require('./model/' + filename).default;
+            const model = moduleFunction(app);
+        })
+    }
+}
+
+loadModel();
---
+++ model/foo.js
@@ -0,0 +1,5 @@
+exports.default = (app) => {
+
+
+    return 's'
+}
