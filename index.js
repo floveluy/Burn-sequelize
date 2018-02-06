9dda862a813a565c8b7066f7cae21d2c5c33da08
Z F <215566435@qq.com>
2018-02-07 09:20:09 +1300

    ➕完成插件

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ module.exports = (app) => {
     const { database, username, password, ...options } = app.config.sequelize;
     app.Sequelize = new Sequelize(database, username, password, options);
     app.model = app.Sequelize.model
-    loadModel(app);
+    loadModel(app, 'app/model');
 }
 
-
-function loadModel(app) {
+function loadModel(app, prefixUrl) {
     const index = __dirname.indexOf('node_modules');
     if (index > 0) {
         const currentDir = __dirname.substr(0, index);
-        readdirSync(currentDir + '/model').forEach((filename) => {
-            const module = require(currentDir + '/model/' + filename).default;
-
-
-        })
-
-    } else {
-        readdirSync('./model').forEach((filename) => {
-            const moduleFunction = require('./model/' + filename).default;
+        var modelObject = {};
+        readdirSync(currentDir + prefixUrl).forEach((filename) => {
+            const moduleFunction = require(currentDir + prefixUrl + '/' + filename).default;
             const model = moduleFunction(app);
+            const modelName = filename.split('.')[0];
+            modelObject[modelName] = model;
         })
+
+        Object.defineProperty(app.context, 'model', {
+            value: modelObject,
+            configurable: false,
+            writable: false
+        })
+
     }
 }
-
-loadModel();
--- model/foo.js
+++
@@ -1,5 +0,0 @@
-exports.default = (app) => {
-
-
-    return 's'
-}
